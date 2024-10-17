3cae1f2e204768b98fa3d0b0e9ea7f20bc3aa102
Joe Yates <joe.g.yates@gmail.com>
2024-10-17 20:36:20 +0100

    Prepare release

--- mix.exs
+++ mix.exs
@@ -5,9 +5,11 @@ defmodule ExDataCatalog.MixProject do
     [
       app: :ex_data_catalog,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.14",
+      description: "A parser for the Data Catalog Vocabulary (DCAT) in Elixir",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -19,7 +21,18 @@ defmodule ExDataCatalog.MixProject do
 
   defp deps do
     [
-      {:sweet_xml, "~> 0.7.4"}
+      {:sweet_xml, "~> 0.7.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/joeyates/ex_data_catalog"
+      },
+      maintainers: ["Joe Yates"]
+    }
   end
 end
